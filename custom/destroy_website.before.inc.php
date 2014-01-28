61caf8d4e9fa5dbff055c8626ddcad7ed8f01ee4
RexDude <rexdude@arcor.de>
2014-01-29 00:51:14 +0100

    Added custom code stuff

--- custom/destroy_website.before.inc.php
+++ custom/destroy_website.before.inc.php
@@ -6,10 +6,10 @@
  * variables you can use:
  * 
  * $sql -> sql object used to perform db operations
- * $websiteId -> website id of website that was destroyed
- * $tablePrefix -> table prefix of website that was destroyed
- * $generatedDir -> generated dir of website that was destroyed
- * $mediaDir -> media dir of website that was destroyed
+ * $websiteId -> website id of website that will be destroyed
+ * $tablePrefix -> table prefix of website that will be destroyed
+ * $generatedDir -> generated dir of website that will be destroyed
+ * $mediaDir -> media dir of website that will be destroyed
  * $log -> logfile object
  *
  * hint: use rex_website_manager_utils::logQuery($log, $sql, 'HERE SQL STATEMENT WITH ' . $tablePrefix . ' USAGE') to log stuff
